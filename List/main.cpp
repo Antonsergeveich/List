99cb3eef500503125517ca9fdc98bf31157898ff
Persh_vx33@top-academy.ru <Persh_vx33@top-academy.ru>
2024-08-02 09:30:40 +0300

    Add project files.

---
+++ List/main.cpp
@@ -0,0 +1,99 @@
+﻿#include<iostream>
+using namespace std;
+
+using std::cin;
+using std::cout;
+using std::endl;
+
+#define tab "\t"
+
+class List
+{
+	class Element
+	{
+		int Data;      //значение элемента
+		Element* pNext;//указательна следующий элемент
+		Element* pPrev;//указатель на предыдущий элемент
+
+	public:
+		Element(int Data, Element* pNext = nullptr, Element* pPrev = nullptr)
+			:Data(Data), pNext(pNext), pPrev(pPrev)
+		{
+			cout << "EConstructor:\t" << this << endl;
+		}
+		~Element()
+		{
+			cout << "EDestructor:\t" << this << endl;
+		}
+		friend class List;
+	}*Head, *Tail;  //объекты класса можно объявлять непосредственно после его описания.
+	//Element* Head;
+	//Element* Tail;
+	size_t size; //typedef unsigned __int64 size_t;
+	//size_t - это typedef на 'unsigned int'.
+	//Директива 'typedef' создаёт псевдоним для существующего типа данных.
+	//typedef имеет следующий синтаксис: typedef существующий_тип_данных псевдоним
+	/*size_t — это специальный беззнаковый целочисленный тип, определённый в стандартных библиотеках языков С и С++.
+		Он является типом результата, возвращаемого оператором sizeof и alignof.Максимально допустимым значением типа size_t является константа SIZE_MAX.
+		Размер size_t выбирается таким образом, чтобы в него можно было записать максимальный размер теоретически возможного массива или объекта.
+		Этот тип используется : для хранения размеров объектов; для индексации массивов; для адресной арифметики.*/
+    //https://cplusplus.com/reference/cstddef/size_t/
+public:
+	List()
+	{
+		Head = Tail = nullptr;
+		size = 0;
+		cout << "LConstructor:\t" << this << endl;
+	}
+	~List()
+	{
+		cout << "LDestructor:\t" << this << endl;
+	}
+
+	//                 Adding elements:
+	void push_front(int Data)
+	{
+		if (Head == nullptr && Tail == nullptr)
+		{
+			Head = Tail = new Element(Data);
+		}
+		else
+		{
+			//1) Создаём новый элемент:
+			Element* New = new Element(Data);
+
+			//2) Привязываем новый элемент к началу списка:
+			New->pNext = Head;
+
+			//3) Привязываем головной элемент списка к новому элементу:
+			Head->pPrev = New;
+
+			//4) Смещаем голову на новый элемент:
+			Head = New;
+		}
+		size++;
+	}
+
+	//                      Methods:
+	void print()const
+	{
+		for (Element* Temp = Head; Temp; Temp = Temp->pNext)
+		{
+			cout << Temp->pPrev << tab << Temp << tab << Temp->Data << tab << Temp->pNext << endl;
+		}
+		cout << "Количество элементов в списке: " << size << endl;
+	}
+};
+
+void main()
+{
+	setlocale(LC_ALL, "");
+	int n;
+	cout << "Введите размер списка: "; cin >> n;
+	List list;
+	for (int i = 0; i < n; i++)
+	{
+		list.push_front(rand() % 100);
+	}
+	list.print();
+}
